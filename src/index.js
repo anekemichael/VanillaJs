4cc4d951ccbe496e00c37fef2c0b4f516c80550f
d1_codes <62024664+anekemichael@users.noreply.github.com>
2021-10-14 15:15:36 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+
+function myFunc(message, favNum) {
+  message = message + " Michael";
+  console.log(message, favNum);
+}
+
+myFunc("Good Morning", 100);
+
+function triplePlus(value) {
+  let newValue = value + 100;
+  return newValue;
+}
+console.log(triplePlus(100));
